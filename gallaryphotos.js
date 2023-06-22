68baa5c5125acc6c3014bb8422676f81f4fb9375
Mounika0404 <137399204+Mounika0404@users.noreply.github.com>
2023-06-22 16:47:08 +0530

    Add files via upload

--- gallaryphotos.js
+++ gallaryphotos.js
@@ -2,7 +2,7 @@ var allImages=document.images
 console.log(allImages)
 for(i=0;i<allImages.length;i++)
 {
-    allImages[i].src=`./Gallary/Photos/${i}.jpg.jpg`
+    allImages[i].src=`./Gallary/${i}.jpg.jpg`
     allImages[i].addEventListener("click",function()
     {
         document.getElementById("bigImgContainer").style.display="flex"
